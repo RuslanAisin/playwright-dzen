eb1b4e53eafa44ae9bcd49e1f6e24f18bec762ee
RuslanAisin <ruslan-spb@mail.ru>
2023-12-22 14:12:33 +0300

    add test
    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+npm i
+npm run test
+allure will open report after test
---
+++ playwright.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "allure-playwright",
+  timeout: 2 * 60 * 1000,
+  use: {
+    launchOptions: {
+      args: ["--start-maximized"],
+    },
+    baseURL: "https://dzen.ru",
+    screenshot: "only-on-failure",
+    actionTimeout: 30000,
+    trace: "off",
+    headless: false,
+    ignoreHTTPSErrors: true,
+    video: {
+      mode: "on",
+      size: { width: 1920, height: 1080 },
+    },
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        viewport: null,
+      },
+    },
+  ],
+});
